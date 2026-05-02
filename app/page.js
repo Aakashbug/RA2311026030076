c8e55716c832d0970450b9d0474773f39c8532c7
Aakashbug <akashbakshi04@gmail.com>
2026-05-02 11:35:17 +0530

    added filter by notification type

--- app/page.js
+++ app/page.js
@@ -5,15 +5,28 @@ import { getNotifications } from "../services/api";
 
 export default function Home() {
   const [data, setData] = useState([]);
+  const [type, setType] = useState("");
 
   useEffect(() => {
     getNotifications().then(setData);
   }, []);
 
+  const filtered = type
+    ? data.filter((n) => n.Type === type)
+    : data;
+
   return (
     <div>
       <h1>Notifications</h1>
-      {data.map((n) => (
+
+      <select onChange={(e) => setType(e.target.value)}>
+        <option value="">All</option>
+        <option value="Event">Event</option>
+        <option value="Result">Result</option>
+        <option value="Placement">Placement</option>
+      </select>
+
+      {filtered.map((n) => (
         <p key={n.ID}>
           {n.Type} - {n.Message}
         </p>
