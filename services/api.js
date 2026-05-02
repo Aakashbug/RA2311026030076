3521d89f208cb504c6c08ac8cf547b6f2995256f
Aakashbug <akashbakshi04@gmail.com>
2026-05-02 11:24:24 +0530

    added API service for notifications

--- services/api.js
+++ services/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
+import { Log } from "../utils/logger";
 
 const BASE_URL = "http://20.207.122.201/evaluation-service";
-
 const TOKEN = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
 
 export const getNotifications = async () => {
   try {
+    Log("frontend", "info", "api", "Fetching notifications");
+
     const res = await axios.get(`${BASE_URL}/notifications`, {
       headers: {
         Authorization: `Bearer ${TOKEN}`
@@ -14,8 +16,8 @@ export const getNotifications = async () => {
 
     return res.data.notifications;
 
-  } catch (error) {
-    console.error("Error fetching notifications");
-    throw error;
+  } catch (err) {
+    Log("frontend", "error", "api", "Failed to fetch notifications");
+    throw err;
   }
 };
